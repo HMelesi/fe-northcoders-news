60aa1a5b7ce186acbec38262bf875145d0795ffd
Harriet Melesi <harrietmelesi@harriets-air.lan>
2020-04-11 19:04:55 +0100

    beginning to work on CSS and collapsible units

---
+++ northcoders-news/src/components/ArticleTitle.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ArticleTitle extends Component {
+  render() {
+    return (
+      <div>
+        <p>fourth article title</p>
+        <p>&#123; votes: 18, comments: 3 &#125;</p>
+      </div>
+    );
+  }
+}
+
+export default ArticleTitle;
